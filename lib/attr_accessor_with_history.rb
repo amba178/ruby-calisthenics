ee6ce32356ee49266d605a31c2093b81fea4c98c
Salem Amba <salemamba@gmail.com>
2015-10-19 11:26:49 -0700

    added comment for @attr_name_history variable

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -4,7 +4,7 @@ class Class
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    # check if the @attr_name_history defined?/eists...if not initialized first
+    # check if the @attr_name_history defined?/exists...if not initialized first
     # than do as usual for setter. 
     
     class_eval %Q{
