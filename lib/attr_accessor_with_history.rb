6cf2ba2bfcd7e05d8d3413047d059a7212db8c34
Salem Amba <salemamba@gmail.com>
2015-10-19 11:25:12 -0700

    added comment for @attr_name_history variable

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -4,8 +4,8 @@ class Class
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    # check if the attr_name_history defined?/ initialized first
-    # than setter instance variable as usual. 
+    # check if the @attr_name_history defined?/eists...if not initialized first
+    # than do as usual for setter. 
     
     class_eval %Q{
 
--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,6 +1,6 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    self.reverse == self 
   end
   def count_words
     # your code here
