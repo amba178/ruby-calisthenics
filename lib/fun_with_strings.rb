d7c008451814a041a2bd1d19cdcd8a525df36572
Salem Amba <salemamba@gmail.com>
2015-10-20 09:22:04 -0700

    done with count_words

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,10 +1,31 @@
 module FunWithStrings
   def palindrome?
-    self.reverse == self 
+    word=self.split("").select{|chr| chr=~/[a-zA-Z]/}.join("").downcase
+    word.reverse == word
+    # byebug
   end
+
   def count_words
     # your code here
+  #   hs={}
+  #   self.split(" ").map{|ch| ch.downcase }.select do |chr| 
+  #     /[a-z]+/.match(chr)==nil ? ch=nil : ch=/[a-z]+/.match(chr)[0]
+  #     if !ch.nil? && hs[ch].nil? 
+  #       hs[ch] = 1
+  #     else 
+  #       if !ch.nil?
+  #        hs[ch]+=1
+  #       end
+  #     end
+  #   end
+  #   return hs 
+  # end
+    arr =self.split(" ").map(&:downcase).map{|chr| /[a-z]+/.match(chr)[0] \
+     if !/[a-z]/.match(chr).nil?}.compact
+    # arr.inject(Hash.new(0)) {|total, e| puts total; total[e]+=1 ;total}
+    arr.inject(Hash.new(0)) {|total, e| total[e]+=1 ;total}
   end
+
   def anagram_groups
     # your code here
   end
