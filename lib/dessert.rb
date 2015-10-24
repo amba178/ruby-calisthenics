2e6adaf570f3428da8324f3e395cbf6f8e63504a
Salem Amba <salemamba@gmail.com>
2015-10-23 21:14:47 -0700

    finished jelly bean class

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,5 +1,7 @@
 class Dessert
+
   attr_accessor :name, :calories 
+
   def initialize(name, calories)
     @name = name
     @calories = calories 
@@ -8,6 +10,7 @@ class Dessert
   def healthy?
     calories < 200 ?  true : false 
   end
+  
   def delicious?
     true 
   end
@@ -15,6 +18,13 @@ end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @flavor = flavor 
+    super("#{flavor} jelly bean", 5)
   end
+
+  def delicious?
+    @flavor=='licorice' ? false : super 
+  end
+
+
 end
